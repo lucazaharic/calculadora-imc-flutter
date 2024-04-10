3298c9db87622e6a7ffec5a8665bb34720ec9b61
lucazaharic <lucazaharic@gmail.com>
2024-04-09 22:27:07 -0300

    Primeiro commit

--- aplicativo_teste/lib/page/calculadora_page.dart
+++ aplicativo_teste/lib/page/calculadora_page.dart
@@ -1,3 +1,5 @@
+import 'package:aplicativo_teste/components/custom_card.dart';
+import 'package:aplicativo_teste/constants.dart';
 import 'package:flutter/material.dart';
 
 class CalculadoraPage extends StatefulWidget {
@@ -10,6 +12,44 @@ class CalculadoraPage extends StatefulWidget {
 class _CalculadoraPageState extends State<CalculadoraPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora IMC"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(child: Row(
+            children: [
+              Expanded(child: CustomCard(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.male,size: 90,),
+                    Text("Masculino",style: labelTextStyle,),
+                  ],
+                ),
+              )),
+              Expanded(child: CustomCard(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.female,size: 90,),
+                    Text("Feminino",style: labelTextStyle,),
+                  ],
+                ),
+              )),
+            ],
+          )),
+          Expanded(child: CustomCard()),
+          Expanded(child:  Row(
+            children: [
+              Expanded(child: CustomCard()),
+              Expanded(child: CustomCard()),
+            ],
+          )),
+        ],
+      ),
+    );
   }
 }

d8a6b1bd5e56e79a14d36ed6b8584c03c38ecfba
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-09 21:41:42 -0300

    Primeiro commit

---
+++ aplicativo_teste/lib/page/calculadora_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculadoraPage extends StatefulWidget {
+  const CalculadoraPage({super.key});
+
+  @override
+  State<CalculadoraPage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<CalculadoraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

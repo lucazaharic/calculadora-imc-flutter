2746bfb511f4dd83ddd5494723478f26a3c0a3f0
lucazaharic <lucazaharic@gmail.com>
2024-04-16 21:29:45 -0300

    Algumas mudanças em aula

--- aplicativo_teste/lib/components/custom_card.dart
+++ aplicativo_teste/lib/components/custom_card.dart
@@ -1,27 +1,20 @@
+import '../constants.dart';
 import 'package:flutter/material.dart';
 
-import '../constants.dart';
-
 class CustomCard extends StatelessWidget {
-
   final Widget? child;
-
-  const CustomCard({super.key,this.child});
+  final bool active;
+  const CustomCard({super.key, this.child, this.active = false});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(7),
+      margin: const EdgeInsets.all(6.0),
       decoration: BoxDecoration(
-        color: activeCardColor,
-        borderRadius: BorderRadius.circular(10),
+        color: active ? activeCardColour : inactiveCardColour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
       child: child,
     );
   }
-
 }
-
-
-
-
--- aplicativo_teste/lib/main.dart
+++ aplicativo_teste/lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:aplicativo_teste/page/calculadora_page.dart';
-import 'package:flutter/material.dart';
-
 import 'constants.dart';
+import 'pages/calculadora_page.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
